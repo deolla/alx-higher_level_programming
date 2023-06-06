c0cac1abc673b72b6adbf76724cb85c789139efe
Adeosun Oluwaseyi <deosundeola@gmail.com>
2023-06-06 08:52:59 -0700

    12-fizzbuzz.py

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def fizzbuzz():
+    for v in range(1,101):
+        if (v % 3 == 0) and (v % 5 == 0):
+            print("FizzBuzz", end=' ')
+        elif v % 3 == 0:
+            print("Fizz", end=' ')
+        elif v % 5 == 0:
+            print("Buzz", end=' ')
+        else:
+            print("{}".format(v),end=' ')
+
+fizzbuzz()
