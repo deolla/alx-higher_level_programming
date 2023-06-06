73d1def1827d02221f9dd355afc56e0c0337847c
Adeosun Oluwaseyi <deosundeola@gmail.com>
2023-06-06 09:00:10 -0700

    12-fizzbuzz.py

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,13 +2,11 @@
 
 def fizzbuzz():
     for v in range(1,101):
-        if (v % 3 == 0) and (v % 5 == 0):
-            print("FizzBuzz", end=' ')
+        if v % 3 == 0 and v % 5 == 0:
+            print("FizzBuzz", end=" ")
         elif v % 3 == 0:
-            print("Fizz", end=' ')
+            print("Fizz", end=" ")
         elif v % 5 == 0:
-            print("Buzz", end=' ')
+            print("Buzz", end=" ")
         else:
-            print("{}".format(v),end=' ')
-
-fizzbuzz()
+            print(v, end=" ")
