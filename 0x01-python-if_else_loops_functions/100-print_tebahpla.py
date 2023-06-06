a310bdcb5cf7d3c237037b7bfe41bb348ec99957
Adeosun Oluwaseyi <deosundeola@gmail.com>
2023-06-06 13:49:26 -0700

    100-print_tebahpla.py

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 
 for m in range(90, 64,- 1):
-    print("{:c}".format(m if m % 2 == 0 else m + 32), end='')
+    n = chr(m)
+    if m % 2 == 0:
+        n = n.lower()
+    print("{}".format(n), end="")
