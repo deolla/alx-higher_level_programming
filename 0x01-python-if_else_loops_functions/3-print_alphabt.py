94fb2951da215d193835d115b195b88e8b21bd8e
Adeosun Oluwaseyi <deosundeola@gmail.com>
2023-06-06 10:00:07 -0700

    3-print_alphabt.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-for n in range(ord('a'), ord('z')):
+for n in range(ord('a'), ord('z')+1):
     if n == ord('e') or n == ord('q'):
         continue
     print("{:c}".format(n), end='')
