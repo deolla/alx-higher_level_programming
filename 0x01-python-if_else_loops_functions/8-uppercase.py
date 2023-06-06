d13eff4674dc4097d4ad8c008ab21722f127bba2
Adeosun Oluwaseyi <deosundeola@gmail.com>
2023-06-06 07:42:42 -0700

    8-uppercase.py

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def uppercase(str):
+    output = ""
+    for m in str:
+        if 97 <= ord(m) <= 122:
+            output += chr(ord(m) - 32)
+        else:
+            output += m
+    print(output)
