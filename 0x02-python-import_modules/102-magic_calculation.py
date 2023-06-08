69c38ca49e81a0486db078e898ffee0f238fca8c
Adeosun Oluwaseyi <deosundeola@gmail.com>
2023-06-08 13:41:26 -0700

    changes

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 
-from magic_calculator_102 import add,sub
-
 def magic_calculation(a, b):
+    from magic_calculator_102 import add, sub
+
     if a < b:
         c = add(a, b)
-        for m in range(4, 7):
+        for m in range(4, 6):
             c = add (c, m)
         return (c)
     else:
-        return sub(a, b)
+        return (sub(a, b))
