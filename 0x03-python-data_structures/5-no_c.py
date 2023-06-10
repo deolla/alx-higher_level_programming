e08778442bf222d0e3f29d319a02ab2d8f3ecce6
Adeosun Oluwaseyi <deosundeola@gmail.com>
2023-06-10 04:17:45 -0700

    changes

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -3,6 +3,6 @@
 def no_c(my_string):
     brand_new_string = ""
     for h in my_string:
-        if h.lower() != 'c':
+        if h != 'c' and h != 'C':
             brand_new_string += h
     return (brand_new_string)
