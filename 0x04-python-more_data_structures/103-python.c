140f398364a9f13539554e0d63f3b68d495cb9ed
Adeosun Oluwaseyi <deosundeola@gmail.com>
2023-06-15 00:07:22 +0000

    changes

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -36,7 +36,6 @@ void print_python_list(PyObject *p)
 	size = PyList_Size(p);
 	printf("[*] Size of the Python List = %ld\n", size);
 	printf("[*] Allocated = %ld\n", list->allocated);
-
 	for (i = 0; i < size; i++)
 	{
 		item = PyList_GetItem(p, i);
@@ -64,9 +63,7 @@ void print_python_list(PyObject *p)
 			printf("int\n");
 		}
 		else if (PyUnicode_Check(item))
-		{
 			printf("str\n");
-		}
 		else
 			printf("%s\n", Py_TYPE(item)->tp_name);
 	}
