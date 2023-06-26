4d793cf70f7a89669e9669203c9764b4ab60e4f9
Adeosun Oluwaseyi Esther <deosundeola@gmail.com>
2023-06-26 09:10:27 -0700

    102-magic_calculation.py

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -3,11 +3,10 @@
 def magic_calculation(a, b):
     score = 0
 
-    for m in range(1, 4):
+    for m in range(1, 3):
         try:
             if m > a:
                 raise Exception('Too far')
-
             score += (a ** b) / m
         except Exception:
             score = b + a
