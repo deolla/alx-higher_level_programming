fe10f689336fec7bf78f6ed300105decb09f1025
Adeosun Oluwaseyi Esther <deosundeola@gmail.com>
2023-06-27 07:29:47 -0700

    0-square.py

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+class Square:
+    """
+    This class represents an empty square.
+    """
+    pass
+
+if __name__ == "__main__":
+    print(f"<class '{__name__}.Square'>")
+    print({})
