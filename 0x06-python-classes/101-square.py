89f1a77ff51727304d7c62549de2d16d5278070d
Adeosun Oluwaseyi Esther <deosundeola@gmail.com>
2023-06-27 12:28:00 -0700

    101-square.py

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -127,7 +127,7 @@ class Square:
             [print("") for i in range(0, self.__position[1])]
         for i in range(0, self.__size):
             [print(" ", end="") for m in range(0, self.__position[0])]
-            [print("#", end="") for l in range(0, self.__size)]
+            [print("#", end="") for p in range(0, self.__size)]
             if i != self.__size - 1:
                 print("")
         return ("")
