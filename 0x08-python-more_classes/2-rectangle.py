ba4e3b2bcfa9f99a725a2953627a0aeb41874c3b
Adeosun Oluwaseyi Esther <deosundeola@gmail.com>
2023-07-03 08:38:41 -0700

    2-rectangle.py

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -98,4 +98,6 @@ class Rectangle:
         Returns:
             int: The parameter of rectangle.
         """
+        if self.__width == 0 or self.__height == 0:
+            return 0
         return self.__width * 2 + self.__height * 2
