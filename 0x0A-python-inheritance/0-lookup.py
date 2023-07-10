25af9f89cbf211238800029722fabb0b37e0ddc3
Adeosun Oluwaseyi Esther <deosundeola@gmail.com>
2023-07-10 08:16:40 -0700

    0-lookup.py

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,11 +1,17 @@
 #!/usr/bin/python3
 
+"""
+Write a function that returns the list of available attributes.
+"""
+
+
 def lookup(obj):
     """
     Returns:
         returns the list of available attributes and methods of an object:
     """
     att_list = []
+
     for m in dir(obj):
         att_list.append(m)
     return att_list
