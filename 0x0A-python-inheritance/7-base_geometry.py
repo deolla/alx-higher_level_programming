cf531450387f62af9c9266fbe41865eb043555ba
Adeosun Oluwaseyi Esther <deosundeola@gmail.com>
2023-07-10 08:45:44 -0700

    7-base_geometry.py

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+"""
+A class BaseGeometry (based on 5-base_geometry.py).
+"""
+class BaseGeometry:
+    """
+    A class BaseGeometry.
+    """
+
+    def area(self):
+        """
+        Raises:
+            Exception: with the message area() is not implemented.
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """
+        Validates values.
+
+        Raises:
+            TypeError: if not integer.
+            ValueError: if not greater than 0.
+        """
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
