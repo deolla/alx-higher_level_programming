4c9081583a9181a1c8394117e8371c8d8e32ecc9
Adeosun Oluwaseyi Esther <deosundeola@gmail.com>
2023-07-11 15:53:09 -0700

    changes

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+A function that returns the dictionary description with simple data structure.
+"""
+
+
+def class_to_json(obj):
+    """
+    returns the dictionary description with simple data structure.
+    
+    Args:
+        obj: object.
+    """
+    pop = {}
+    if hasattr(obj, "__dict__"):
+        pop = obj.__dict__.copy()
+    return pop
