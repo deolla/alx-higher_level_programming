853d119d37e6f66224988199a6c02a4b6488d30d
Adeosun Oluwaseyi Esther <deosundeola@gmail.com>
2023-07-11 07:32:27 -0700

    changes

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -8,6 +8,9 @@ def read_file(filename=""):
     """
     Reads a text file (UTF8) and prints it to stdout:
     """
+    if not filename:
+        print("No filename provided.")
+        return
     with open(filename, 'r', encoding='utf-8') as file:
         read_text_file = file.read()
         print(read_text_file)
