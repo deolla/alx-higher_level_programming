4ad6ec347120f10ce661da4b17ed9a4dfb1d00ff
Adeosun Oluwaseyi Esther <deosundeola@gmail.com>
2023-07-11 07:23:11 -0700

    0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -10,4 +10,4 @@ def read_file(filename=""):
     """
     with open(filename, encoding='utf-8') as f:
         read_text_file = f.read()
-        print(read_text_file)
+        return(read_text_file)
