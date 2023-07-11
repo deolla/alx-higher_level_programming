58ee58e31ef16e245191d20364bcb4799d70545e
Adeosun Oluwaseyi Esther <deosundeola@gmail.com>
2023-07-11 13:45:32 -0700

    3-to_json_string.py

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+A function that returns the JSON representation of an object (string)
+"""
+
+
+def to_json_string(my_obj):
+    """
+    returns the JSON representation of an object (string).
+
+    Args:
+        my_obj: the object.
+
+    Returns:
+        returns the JSON representation.
+    """
+    return json.dumps(my_obj)
