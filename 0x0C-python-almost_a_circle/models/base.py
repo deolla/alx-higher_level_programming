265b5eecfb1f472f08b2a6a336e833390709e91a
Adeosun Oluwaseyi Esther <deosundeola@gmail.com>
2023-07-13 05:32:37 -0700

    Base class

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+"""
+This class will be the “base” of all other classes in this project.
+"""
+
+
+class Base:
+    """
+    This class is "base" of all classes in this project.
+
+    Attribute:
+        __nb_object: private class attribute.
+        id:  public instance attribute.
+    """
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """
+        Initialise the "base" instance with an optional id.
+
+        Args:
+            id (int): This is a unique identifier of the base.
+
+        Raises:
+            Exceptions: 
+        """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
