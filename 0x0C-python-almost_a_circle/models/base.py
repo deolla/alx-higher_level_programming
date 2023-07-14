f35947af94c1b2e0bbe675068f55ffd25da98721
Adeosun Oluwaseyi Esther <deosundeola@gmail.com>
2023-07-14 07:22:51 -0700

    python-almost_a_circle

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -3,6 +3,7 @@
 """
 This class will be the “base” of all other classes in this project.
 """
+import json
 
 
 class Base:
@@ -27,3 +28,46 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """
+        Returns:
+            returns the JSON string representation of list_dictionaries.
+
+        Args:
+            list_dictionaries: a list of dictionaries.
+        """
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """
+        Writes the JSON string representation of list_objs to a file.
+
+        Args:
+            list_objs: a list of instances who inherits of Base.
+        """
+        if  list_objs is None:
+            list_objs = "[]"
+
+        filename = cls.__name__ + ".json"
+        with open(filename, 'w') as f:
+            json_string = cls.to_json_string([obj.to_dictionary() for obj in list_objs])
+            f.write(json_string)
+
+    @staticmethod
+    def from_json_string(json_string):
+        """
+        Returns:
+            returns the list of the JSON string representation json_string.
+
+        Args:
+            json_string: a string representing a list of dictionaries.
+        """
+        if json_string is None or json_string == "":
+            return "[]"
+        return json.loads(json_string)
+
