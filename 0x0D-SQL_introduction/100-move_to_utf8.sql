dd3fb101f3ac9a40db99620c3f223c2205c3f86f
Adeosun Oluwaseyi <deosundeola@gmail.com>
2023-08-15 11:19:12 -0700

    changes

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,12 +1,6 @@
 -- Script: Convert specified elements to UTF8 in MySQL server
 
 -- Task: Convert database hbtn_0c_0 to UTF8
-USE hbtn_0c_0;
-
-ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
--- Task: Convert table first_table to UTF8
-ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
--- Task: Convert field name in table first_table to UTF8
-ALTER TABLE first_table CHANGE name name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+USE `hbtn_0c_0`
+ALTER TABLE `first_table`
+CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
