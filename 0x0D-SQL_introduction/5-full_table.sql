4d17942f3eb2e1253bc1f245f03828cfd2436b03
Adeosun Oluwaseyi <deosundeola@gmail.com>
2023-08-15 07:55:23 -0700

    changes

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,4 +1,2 @@
 -- A script that lists all rows of the table first_table from the database hbtn_0c_0 in your MySQL server.
-SELECT COLUMN_NAME, DATA_TYPE, CHARACTER_MAXIMUM_LENGTH
-FROM information_schema.columns
-WHERE TABLE_SCHEMA = 'hbtn_0c_0' AND TABLE_NAME = 'first_table';
+SHOW CREATE TABLE first_table from htbn_0c_0
