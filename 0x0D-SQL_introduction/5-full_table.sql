9ee553c5ee55d306b64e46266bd308847e586436
Adeosun Oluwaseyi <deosundeola@gmail.com>
2023-08-15 07:56:46 -0700

    changes

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,2 +1,2 @@
 -- A script that lists all rows of the table first_table from the database hbtn_0c_0 in your MySQL server.
-SHOW CREATE TABLE first_table FROM htbn_0c_0
+SHOW CREATE TABLE first_table
