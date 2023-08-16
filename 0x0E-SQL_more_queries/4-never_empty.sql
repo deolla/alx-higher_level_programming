f92d683802d45520b0af378d5d78efdd09339c46
Adeosun Oluwaseyi <deosundeola@gmail.com>
2023-08-16 07:33:30 -0700

    changes

--- 0x0E-SQL_more_queries/4-never_empty.sql
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -1,6 +1,5 @@
 -- creates the table id_not_null on your MySQL server.
-USE hbtn_0d_2;
 CREATE TABLE IF NOT EXISTS id_not_null(
-	id INT NOT NULL DEFAULT 1,
+	id INT DEFAULT 1,
 	name VARCHAR(256)
 );
