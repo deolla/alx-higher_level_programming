aab7ca58d575b882eb617e962738256d8f57c09f
Adeosun Oluwaseyi <deosundeola@gmail.com>
2023-08-16 14:32:36 -0700

    SQL

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -2,6 +2,6 @@
 SELECT CONCAT(tv_genres.name, ' - ', SUM(tv_shows.rating)) AS genre_rating
 FROM tv_genres
 JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
-JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+JOIN tv_show_ratings ON tv_show_genres.show_id = tv_show_ratings.show_id
 GROUP BY tv_genres.name
 ORDER BY SUM(tv_shows.rating) DESC;
