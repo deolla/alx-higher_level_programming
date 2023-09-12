e40284e424cc3b2e0d9566015a6ffb7081ec992c
Adeosun Oluwaseyi <deosundeola@gmail.com>
2023-09-12 11:42:36 -0700

    100-map.js

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,10 +1,6 @@
 #!/usr/bin/node
-// Write a script that imports an array and computes a new array.
-// Your script must import list from the file 100-data.js
-// You must use a map
-// A new list must be created with each value equal to the value of the initial list, multipled by the index in the list
-// Print both the initial list and the new list
+//Write a script that imports an array and computes a new array.
 
-const arr = require('./100-data.js').arr;
-console.log(arr);
-console.log(arr.map((item, index) => value * index));
+const list = require('./100-data.js').list;
+console.log(list);
+console.log(list.map((value, index) => value * index));
