f994b96541cbc92bd33206acce921c7819d5331d
Adeosun Oluwaseyi <deosundeola@gmail.com>
2023-09-12 08:02:12 -0700

    2-rectangle.js

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -3,13 +3,11 @@
 // If w or h is equal to 0 or not a positive integer, create an empty object.
 
 class Rectangle {
-  constructor(w, h) {
-    if (w <= 0 || h <= 0 || typeof w !== 'number' || typeof h !== 'number') {
-      // Create an empty object if w or h is 0 or not a positive integer
-      return {};
+  constructor (w, h) {
+    if ((w > 0) && (h > 0)) {
+      this.width = w;
+      this.height = h;
     }
-    this.width = w;
-    this.height = h;
   }
 }
 module.exports = Rectangle;
