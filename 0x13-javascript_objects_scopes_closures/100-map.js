80ee1cbac021922f97724a31be6c3a00b0960d25
Adeosun Oluwaseyi <deosundeola@gmail.com>
2023-09-12 12:14:33 -0700

    100-map.js

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-//Write a script that imports an array and computes a new array.
+// Write a script that imports an array and computes a new array.
 
 const list = require('./100-data.js').list;
 console.log(list);
