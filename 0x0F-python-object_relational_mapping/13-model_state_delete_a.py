5d6a43911f2fad29a2081d889303426ac5964cf5
Adeosun Oluwaseyi <deosundeola@gmail.com>
2023-09-15 10:46:18 -0700

    13-model_state_delete_a.py

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -15,7 +15,7 @@ if __name__ == "__main__":
     Base.metadata.create_all(engine)
     session = Session(engine)
 
-     for state in session.query(State).filter(State.name.like('%a%')).all():
-         session.delete(state)
+    for state in session.query(State).filter(State.name.like('%a%')).all():
+        session.delete(state)
 
     session.commit()
