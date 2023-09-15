e561542d548f1a28d76e91d91ea07201b74e3635
Adeosun Oluwaseyi <deosundeola@gmail.com>
2023-09-15 08:19:25 -0700

    task_6

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -4,7 +4,7 @@ Write a python file that contains the class definition of a State
 and an instance Base = declarative_base()
 """
 from sqlalchemy import Column, String, Integer, Sequence
-from sqlalchemy.ext.declarative  import declarative_base
+from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
