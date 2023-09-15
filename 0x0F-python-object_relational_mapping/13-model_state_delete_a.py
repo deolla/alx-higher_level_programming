a0f952c75fe76ddab5edab9d04876a39983c54f0
Adeosun Oluwaseyi <deosundeola@gmail.com>
2023-09-15 10:44:46 -0700

    12-model_state_update_id_2.py

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """
-Write a script that adds the State object “Louisiana” to the
-database hbtn_0e_6_usa
+Write a script that deletes all State objects with a name containing the
+letter a from the database hbtn_0e_6_usa.
 """
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import Session
 from model_state import Base, State
 import sys
 
@@ -12,9 +12,10 @@ import sys
 if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
              sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
-    Session = sessionmaker(bind=engine)
-    session = Session()
     Base.metadata.create_all(engine)
-    m = session.query(State).filter_by(id=2).first()
-    m.name = 'New Mexico'
+    session = Session(engine)
+
+     for state in session.query(State).filter(State.name.like('%a%')).all():
+         session.delete(state)
+
     session.commit()
