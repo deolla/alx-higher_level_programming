e93cf8c65cc0060d9da55b6910103ef63714daa8
Adeosun Oluwaseyi <deosundeola@gmail.com>
2023-09-29 05:11:41 -0700

    python_network

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Takes in a URL, sends a request to the URL."""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    response = requests.get(url)
+    html = reponse.text
+
+    if response.status_code >= 400:
+        print(f"Error code: {response.status_code}")
+    else:
+        print(html)
