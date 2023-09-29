203b046251ec7aa295569cfa584ab62f67d3a031
Adeosun Oluwaseyi <deosundeola@gmail.com>
2023-09-29 04:18:56 -0700

    python_network

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,3 +5,9 @@ import urllib.request
 
 with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
     html = response.read()
+    content = html.decode('utf-8')
+
+print("Body response:")
+print("\t- type: {}".format(type(html)))
+print("\t- content: {}".format(html))
+print("\t- utf8 content: {}".format(content))
