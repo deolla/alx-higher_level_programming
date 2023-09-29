96f3c8d627d8823672f41d8923d79cbb2adbaf41
Adeosun Oluwaseyi <deosundeola@gmail.com>
2023-09-29 05:05:44 -0700

    python_network

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""akes in a URL, sends a request to the URL and displays the value of the variable X-Request-Id"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    response = requests.get(url)
+    req_id = response.headers.get('X-Request-Id')
+    print(req_id)
