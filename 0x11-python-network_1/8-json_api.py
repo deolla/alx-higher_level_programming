8e7554db98565f5146787405450911f6e59f48d3
Adeosun Oluwaseyi <deosundeola@gmail.com>
2023-09-29 05:19:14 -0700

    python_network

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -6,14 +6,17 @@ import sys
 if __name__ == "__main__":
     letter = sys.argv[1] if len(sys.argv) > 1 else ""
     url = 'http://0.0.0.0:5000/search_user'
-    
+
     data = {'q': letter}
     response = requests.post(url, data=data)
 
     try:
         json_response = response.json()
         if json_response:
-            print("[{}] {}".format(json_response.get('id'), json_response.get('name')))
+            print("[{}] {}".format(
+                json_response.get('id'),
+                json_response.get('name')
+            ))
         else:
             print("No result")
     except ValueError:
