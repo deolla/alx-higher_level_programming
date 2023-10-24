8c40a6a35d48c42ec15fab04842a2084b7af9415
Adeosun Oluwaseyi <deosundeola@gmail.com>
2023-10-24 11:39:17 -0700

    web_scraping

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -7,18 +7,46 @@ const characterId = 18;
 
 request.get(apiUrl, (error, response, body) => {
   if (error) {
-    console.error(error);
+    console.log(error);
   } else if (response.statusCode !== 200) {
-    console.error(response.statusCode);
+    console.log(response.statusCode);
   } else {
     try {
       const films = JSON.parse(body).results;
-      const moviesWithWedgeAntilles = films.filter((film) =>
-        film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`)
-      );
-      console.log(moviesWithWedgeAntilles.length);
+      let movieCount = 0;
+
+      const fetchCharacter = (url) => {
+        return new Promise((resolve, reject) => {
+          request.get(url, (error, response, body) => {
+            if (error) {
+              reject(error);
+            } else if (response.statusCode !== 200) {
+              reject(new Error(response.statusCode));
+            } else {
+              const character = JSON.parse(body);
+              resolve(character);
+            }
+          });
+        });
+      };
+
+      const checkMovies = async () => {
+        for (const film of films) {
+          const characters = film.characters;
+          for (const characterUrl of characters) {
+            const character = await fetchCharacter(characterUrl);
+            if (character.url === `https://swapi-api.alx-tools.com/api/people/${characterId}/`) {
+              movieCount++;
+              break;
+            }
+          }
+        }
+        console.log(movieCount);
+      };
+
+      checkMovies();
     } catch (error) {
-      console.error(error);
+      console.log(error);
     }
   }
 });
